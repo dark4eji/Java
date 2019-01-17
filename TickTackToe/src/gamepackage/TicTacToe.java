f62a54fea2ed8e1168efe7e1f28d8a345905b961
dark4eji <dark4eji@gmail.com>
2019-01-18 00:04:03 +0700

    Finish tic tac toe

--- TickTackToe/src/gamepackage/TicTacToe.java
+++ TickTackToe/src/gamepackage/TicTacToe.java
@@ -10,42 +10,37 @@ public class TicTacToe {
     private static char COMP_CHAR = 'O';
     private static char DOT_EMPTY = '•';
 
-    private static int SIZE = 4;
-    private static int turn_count = 0;
-
-    private static boolean win_state = false;
+    private static final int SIZE = 3;
 
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random();
 
     public static void main( String[] args ) {
+        int[] coordinatesBuffer = new int[2];  // Хранит координаты точки: x = [0], y = [1]
+        int turnsCounter = 0;
+
         initMap();
         printMap();
 
         while(true) {
+            turnsCounter++;
 
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
+            humanTurn(coordinatesBuffer);
+            if ( turnsCounter >= SIZE ) {
+                if ( isEndGame(HUMAN_CHAR, coordinatesBuffer) ) {
+                    break;
+                }
             }
 
-            humanTurn();
-            if (win_state) {
-                System.out.println("Победил человек");
-                break;
-            }
-
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
-            }
-
-            computerTurn();
-            if (win_state) {
-                System.out.println("Победил компьютер");
-                break;
+            computerTurn(coordinatesBuffer);
+            if ( turnsCounter >= SIZE ) {
+                if ( isEndGame(COMP_CHAR, coordinatesBuffer) ) {
+                    break;
+                }
             }
         }
+
+        System.out.println("Игра закончена");
     }
 //-----------------------------------------------------------------------------------
     public static void initMap() {
@@ -76,49 +71,114 @@ public class TicTacToe {
         System.out.println();
     }
 //-----------------------------------------------------------------------------------
-    public static void humanTurn() {
-        int x, y;
-
+    public static void humanTurn(int[] coordinatesBuffer) {
         do {
             System.out.print( "Введите координаты через пробел: " );
-            x = scanner.nextInt() - 1;
-            y = scanner.nextInt() - 1;
-        } while ( !isCellValid(x, y) );
+            coordinatesBuffer[0] = scanner.nextInt() - 1;
+            coordinatesBuffer[1] = scanner.nextInt() - 1;
+        } while ( !isCellValid ( coordinatesBuffer[0], coordinatesBuffer[1] ));
 
-        addCheckChar( HUMAN_CHAR, x, y );
+        setsDataAndPrintMap(HUMAN_CHAR, coordinatesBuffer);
     }
 //-----------------------------------------------------------------------------------
-    private static void computerTurn() {
-        int x, y;
+    private static void computerTurn( int[] coordinatesBuffer ) {
+        int maxScoreFieldX = -1;
+        int maxScoreFieldY = -1;
+        int maxScore = 0;
 
-        do {
-            x = random.nextInt( SIZE );
-            y = random.nextInt( SIZE );
-        } while ( !isCellValid(x, y) );
+        for ( int i = 0; i < SIZE; i++ ) {
+            for ( int j = 0; j < SIZE; j++ ) {
+                int fieldScore = 0;
 
-        System.out.println("Компьютер выбрал ячейку " + ( y + 1 ) + " " + ( x + 1 ));
+                if ( map[i][j] == DOT_EMPTY ) {
 
-        addCheckChar( COMP_CHAR, x, y );
-    }
-//-----------------------------------------------------------------------------------
-    private static void aiEvaluator(){
-        for(int i = 0; i < SIZE; i++){
-            for(int j = 0; j < SIZE; j++){
+                    if ( i - 1 >= 0 && j - 1 >= 0 && map[i - 1][j - 1] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
 
+                    if ( i + 1 < SIZE && j - 1 >= 0 && map[i + 1][j - 1] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+
+                    if ( i - 1 >= 0 && j + 1 < SIZE && map[i - 1][j + 1] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+
+                    if ( i + 1 < SIZE && j + 1 < SIZE && map[i + 1][j + 1] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+
+                    if ( i - 1 >= 0 && map[i - 1][j] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+
+                    if ( j - 1 >= 0 && map[i][j - 1] == COMP_CHAR) {
+                        fieldScore++;
+                    }
+
+                    if ( j + 1 < SIZE && map[i][j + 1] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+
+                    if ( i + 1 < SIZE && map[i + 1][j] == COMP_CHAR ) {
+                        fieldScore++;
+                    }
+                }
+
+                if ( fieldScore > maxScore ) {
+                    maxScore = fieldScore;
+                    maxScoreFieldX = i;
+                    maxScoreFieldY = j;
+                }
             }
         }
 
-    }
-//-----------------------------------------------------------------------------------
-    private static void addCheckChar( char playerChar, int x, int y ) {
-        turn_count += 1;
-        map[x][y] = playerChar;
+        if ( maxScoreFieldX != -1 ) {
+            coordinatesBuffer[0] = maxScoreFieldX;
+            coordinatesBuffer[1] = maxScoreFieldY;
+        }
+
+        if ( maxScoreFieldX == -1 ) {
+            System.out.println("kek");
+            do {
+                coordinatesBuffer[0] = random.nextInt(SIZE);
+                coordinatesBuffer[1] = random.nextInt(SIZE);
+            } while (!isCellValid(coordinatesBuffer[0], coordinatesBuffer[1]));
+        }
+
+        map[coordinatesBuffer[0]][coordinatesBuffer[1]] = COMP_CHAR;
 
         printMap();
 
-        if ( turn_count >= SIZE ) {
-            checkWin(playerChar, x, y);
+        System.out.println("Компьютер выбрал ячейку " + ( coordinatesBuffer[0] + 1 )
+                + " " + ( coordinatesBuffer[1] + 1 ));
+    }
+//-----------------------------------------------------------------------------------
+    private static void setsDataAndPrintMap( char playerChar,
+                                           int[] coordinatesBuffer ) {
+        map[coordinatesBuffer[0]][coordinatesBuffer[1]] = playerChar;
+
+        printMap();
+    }
+// ----------------------------------------------------------------------------------
+    private static boolean isEndGame( char playerSymbol,
+                                      int[] coordinatesBuffer ){
+        boolean result = false;
+
+        if ( checkWin(playerSymbol, coordinatesBuffer) ) {
+            System.out.println("Победили " + playerSymbol);
+            result = true;
         }
+
+        if (result) {
+            return result;
+        }
+
+        if (isMapFull()) {
+            System.out.println("Ничья");
+        }
+
+        return result;
     }
 //-----------------------------------------------------------------------------------
     private static boolean isCellValid( int x, int y ) {
@@ -137,28 +197,36 @@ public class TicTacToe {
     }
 //-----------------------------------------------------------------------------------
     private static boolean isMapFull() {
-        boolean result = false;
-        int freeCells = SIZE * SIZE;
+        boolean result = true;
+
         for ( int i = 0; i < SIZE; i++ ) {
-            for ( int j = 0; j < SIZE; j++ ) {
-                freeCells = ( map[i][j] != DOT_EMPTY ) ? freeCells - 1 : freeCells;
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    result = false;
+                    break;
+                }
+            }
+            if (!result) {
+                break;
             }
         }
-        result = ( freeCells == 0 );
+
         return result;
     }
 //-----------------------------------------------------------------------------------
-    private static void checkWin( char playerSymbol, int x, int y ) {
+    private static boolean checkWin( char playerSymbol, int[] coordinatesBuffer ) {
         /** Метод выполняет проверку ходов на выигрыш/отсутствие условия выигрыша как человка, так и компьютера */
+        boolean result = false;
         int[] count = { 0, 0, 0, 0 };
 
         for ( int i = 0; i < SIZE; i++ ) {
             count[0] = ( map[i][i] == playerSymbol ) ? count[0] + 1 : count[0];
             count[1] = ( map[i][(map[i].length - 1 ) - i] == playerSymbol) ? count[1] + 1 : count[1];
-            count[2] = ( map[x][i] == playerSymbol ) ? count[2] + 1 : count[2];
-            count[3] = ( map[i][y] == playerSymbol ) ? count[3] + 1 : count[3];
+            count[2] = ( map[coordinatesBuffer[0]][i] == playerSymbol ) ? count[2] + 1 : count[2];
+            count[3] = ( map[i][coordinatesBuffer[1]] == playerSymbol ) ? count[3] + 1 : count[3];
         }
 
-        win_state = ( count[0] == SIZE || count[1] == SIZE || count[2] == SIZE || count[3] == SIZE );
+        result = ( count[0] == SIZE || count[1] == SIZE || count[2] == SIZE || count[3] == SIZE );
+        return result;
     }
 }
